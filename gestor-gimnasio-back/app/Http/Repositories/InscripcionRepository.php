ae06fa50fb37af6e23dd161cb114c6985467e226
Facundo Ortega <facuortega21@gmail.com>
2025-05-23 17:30:14 -0300

    feat: crear métodos para inscribir un usuario a una clase

---
+++ gestor-gimnasio-back/app/Http/Repositories/InscripcionRepository.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Http\Repositories;
+
+use App\Http\Interfaces\InscripcionRepositoryInterface;
+use App\Models\Inscripcion;
+
+class InscripcionRepository implements InscripcionRepositoryInterface
+{
+    public function inscribirUsuario($id_usuario, $id_turno_clase)
+    {
+        return Inscripcion::create([
+            'id_usuario' => $id_usuario,
+            'id_turno_clase' => $id_turno_clase,
+            'fecha' => now(),
+        ]);
+    }
+}
--- gestor-gimnasio-back/routes/api.php
+++ gestor-gimnasio-back/routes/api.php
@@ -1,27 +1,39 @@
 <?php
 
 use App\Http\Controllers\AuthController;
+use App\Http\Controllers\Inscripcion;
 use App\Http\Controllers\TurnoClase;
 use App\Http\Controllers\UsuarioController;
 use Illuminate\Support\Facades\Route;
 
+const AUTH_SANCTION = 'auth:sanctum';
+
 Route::post('auth/login', [AuthController::class, 'login']);
+
 Route::prefix('usuarios')->group(function () {
     Route::post('/', [UsuarioController::class, 'store'])->name('usuarios.store');
     Route::get('/check-email/{email}', [UsuarioController::class, 'checkEmailExists'])
         ->middleware('throttle:5,1')
         ->name('usuarios.checkEmail');
 
-    Route::middleware('auth:sanctum')->group(function () {
+    Route::middleware(AUTH_SANCTION)->group(function () {
         Route::get('/', [UsuarioController::class, 'index'])->name('usuarios.index');
         Route::get('/{id}', [UsuarioController::class, 'show'])->name('usuarios.show');
     });
 });
+
 Route::prefix('turnos-clase')->group(function () {
-    Route::middleware('auth:sanctum')->group(function () {
+    Route::middleware(AUTH_SANCTION)->group(function () {
         Route::get('/user-inscription-status/{userId}', [TurnoClase::class, 'getAllWithUserInscriptionStatus'])
             ->name('turnos-clase.user-inscription-status');
         Route::get('/cupo-maximo/{idTurnoClase}', [TurnoClase::class, 'getCupoMaximoFromTurnoClase'])
             ->name('turnos-clase.cupo-maximo');
+    });
+});
+
+Route::prefix('inscripciones')->group(function () {
+    Route::middleware(AUTH_SANCTION)->group(function () {
+        Route::post('/', [Inscripcion::class, 'inscribirUsuario'])
+            ->name('inscripciones.inscribir-usuario');
     });
 });
