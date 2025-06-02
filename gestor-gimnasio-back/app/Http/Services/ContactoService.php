5a73983f59d927b49ebd306c49fa79799b308f3a
Facundo Ortega <facuortega21@gmail.com>
2025-06-02 09:43:58 -0300

    feat: simular envio de email al mail de administrador

--- gestor-gimnasio-back/app/Http/Services/ContactoService.php
+++ gestor-gimnasio-back/app/Http/Services/ContactoService.php
@@ -5,6 +5,8 @@ namespace App\Http\Services;
 use App\Http\Interfaces\ContactoServiceInterface;
 use App\Http\Interfaces\ContactoRepositoryInterface;
 use App\Models\Contacto;
+use Illuminate\Support\Facades\Log;
+use Illuminate\Support\Facades\Mail;
 
 class ContactoService implements ContactoServiceInterface
 {
@@ -20,6 +22,36 @@ class ContactoService implements ContactoServiceInterface
      */
     public function create(array $data)
     {
-        return $this->contacto_repository->create($data);
+        $contacto = $this->contacto_repository->create($data);
+        $this->enviarEmailNotificacionAdmin($contacto);
+        return $contacto;
+    }
+
+    /**
+     * Envia un email de notificación al administrador del gimnasio
+     *
+     * @param Contacto $contacto
+     * @return void
+     */
+    private function enviarEmailNotificacionAdmin(Contacto $contacto)
+    {
+        try {
+            Mail::raw(
+                "Nuevo mensaje de contacto:\n" .
+                    "De: {$contacto->email}\n" .
+                    "Asunto: {$contacto->asunto}\n" .
+                    "Mensaje: {$contacto->mensaje}\n" .
+                    "Fecha: " . now()->format('d/m/Y H:i:s'),
+                function ($message) use ($contacto) {
+                    $message->to('admin@fitmanager.com')
+                        ->subject($contacto->asunto)
+                        ->replyTo($contacto->email);
+                }
+            );
+
+            Log::info("Email de contacto enviado para contacto ID: {$contacto->id}");
+        } catch (\Exception $e) {
+            Log::error("Error enviando email de contacto: " . $e->getMessage());
+        }
     }
 }
