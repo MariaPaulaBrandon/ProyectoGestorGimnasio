c3e2a6be4d39ecbedcbffa9caa9a5bfeda0991c2
Maria Paula Brandon <m.paulabrandon@gmail.com>
2025-06-13 02:02:51 -0300

    fix: Update contactoLanding route to accept request data for email

--- gestor-gimnasio-back/routes/web.php
+++ gestor-gimnasio-back/routes/web.php
@@ -4,8 +4,8 @@ use Illuminate\Support\Facades\Route;
 use Illuminate\Support\Facades\Mail;
 use App\Mail\ContactoLandingMail;
 
-Route::post('/contactoLanding', function () {
-    $datos = ['nombre' => 'Funny Coder'];
-    Mail::to('fitmanagersrl@gmail.com')->send(new ContactoLandingMail($datos['nombre']));
+Route::post('/contactoLanding', function (\Illuminate\Http\Request $request) {
+    $datos = $request->all(); // o seleccioná solo los campos que quieras
+    Mail::to('fitmanagersrl@gmail.com')->send(new ContactoLandingMail($datos));
     return 'Mail enviado';
 });
